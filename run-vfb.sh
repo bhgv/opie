563f1da8b3f614e5b316ce03c0e04d4718f91dbf
bhgv <bhgv.empire@gmail.com>
2018-09-12 13:47:11 +0300

    different run-scripts for start vfb and for run opie

---
+++ run-vfb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export QTDIR=$PWD/../qte-opie
+
+export OPIEDIR=$PWD
+
+export LD_LIBRARY_PATH=$OPIEDIR/lib:$QTDIR/lib:$LD_LIBRARY_PATH
+
+export PATH=$QTDIR/bin:$OPIEDIR/bin:$PATH
+
+export QWS_DISPLAY=QVFb:0
+
+qvfb&
+
+#qpe -qws
+
